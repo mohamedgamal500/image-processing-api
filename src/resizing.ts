4ae000dd619aa26d9064e3f6b56e2ef4cb03e44a
Mohamed Gamal <mohamedgamalmohamed800@gmail.com>
2022-01-23 00:48:12 +0200

    Update resizing.ts

--- src/resizing.ts
+++ src/resizing.ts
@@ -1,7 +1,7 @@
 import sharp from "sharp";
 
-export const resizeImage = async (width: number, height: number, source: string, output: string) => {
+export const resizeImage = (width: number, height: number, source: string, output: string) => {
 
-    return await sharp(source).resize(width, height).toFile(output)
+    return sharp(source).resize(width, height).toFile(output)
 
 }
