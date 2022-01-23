659608dd3599d77bc76228252b4faa08ec008a70
Mohamed Gamal <mohamedgamalmohamed800@gmail.com>
2022-01-23 03:34:07 +0200

    handle caching with file system

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Application, Request, Response } from 'express'
 import logger from 'morgan'
 import { resizeImage } from './resizing'
+import fs from "fs";
+
 
 
 const PORT = 5000
@@ -10,26 +12,33 @@ app.use(logger('dev'))
 
 
 app.get('/', async (req: Request, res: Response) => {
-  const width: number = Number(req.query['width']);
-  const height: number = Number(req.query['height']);
-  const imageName: string = String(req.query['imageName']);
+  const width: number = Number(req.query['width'])
+  const height: number = Number(req.query['height'])
+  const imageName: string = String(req.query['imageName'])
   const source: string = `images/${imageName}.jpg`
   const output: string = `thumbnails/${imageName}_thumbnail_${width}X${height}.jpg`
 
-  try {
-    await resizeImage(width, height, source, output)
-    res.json({
-      message: 'Done'
-    })
-  }
-  catch (err) {
-    console.log('sharp error', err)
-    res.status(500)
-    res.json({
-      message: 'Failed'
-    })
-  }
 
+  fs.readFile(output, async (err, content) => {
+    try {
+      if (err) {
+        console.log("No such thumb image")
+        await resizeImage(width, height, source, output)
+        fs.readFile(output, (err, content) => {
+          res.end(content)
+        })
+      } else {
+        res.end(content)
+      }
+    }
+    catch (err) {
+      console.log(err)
+      res.status(500)
+      res.json({
+        message: 'Faile To Resize The Img'
+      })
+    }
+  })
 })
 
 app.listen(PORT, () => {
